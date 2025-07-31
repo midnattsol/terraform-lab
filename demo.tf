f4a058ac07bbb1e6c47bc07ecc0f311dcf276947
midnattsol <juanjo@delasheras.dev>
2025-08-01 00:30:56 +0200

    test

---
+++ demo.tf
@@ -0,0 +1,13 @@
+# main.tf
+terraform {
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my-terraform-test-bucket-1234567890"
+  acl    = "private"
+}
