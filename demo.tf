a322eb16ec7bfb28b6624bd52a6a471ce65e10c5
midnattsol <juanjo@delasheras.dev>
2025-08-01 00:33:31 +0200

    test

--- demo.tf
+++ demo.tf
@@ -8,6 +8,6 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "test_bucket" {
-  bucket = "my-terraform-test-bucket-1234567890"
+  bucket = "my-terraform-test-bucket-1234567891"
   acl    = "private"
 }
